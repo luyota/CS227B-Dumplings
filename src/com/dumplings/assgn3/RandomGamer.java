4199641feb04f95df6dffd1170612331b8855f66
Alexander Blessing <abless@dn0a215337.sunet>
2011-04-16 15:40:24 -0700

    Added RandomGamer

---
+++ src/com/dumplings/assgn3/RandomGamer.java
@@ -0,0 +1,70 @@
+package com.dumplings.assgn3;
+
+import java.util.List;
+import java.util.Random;
+
+import player.gamer.statemachine.StateMachineGamer;
+import player.gamer.statemachine.reflex.event.ReflexMoveSelectionEvent;
+import player.gamer.statemachine.reflex.gui.ReflexDetailPanel;
+import util.statemachine.Move;
+import util.statemachine.StateMachine;
+import util.statemachine.exceptions.GoalDefinitionException;
+import util.statemachine.exceptions.MoveDefinitionException;
+import util.statemachine.exceptions.TransitionDefinitionException;
+import util.statemachine.implementation.prover.ProverStateMachine;
+import apps.player.detail.DetailPanel;
+
+/**
+ * RandomGamer plays a random legal move
+ */
+public final class RandomGamer extends StateMachineGamer
+{
+	
+	@Override
+	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// Do nothing.
+	}
+	
+	/**
+	 * Selects a random legal move
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		long start = System.currentTimeMillis();
+		Random generator = new Random();
+		
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move selection = moves.get(generator.nextInt(moves.size()));
+
+		long stop = System.currentTimeMillis();
+
+		notifyObservers(new ReflexMoveSelectionEvent(moves, selection, stop - start));
+		return selection;
+	}
+	
+	@Override
+	public void stateMachineStop() {
+		// Do nothing.
+	}
+
+	/**
+	 * Uses a ProverStateMachine
+	 */
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new ProverStateMachine();
+	}
+	@Override
+	public String getName() {
+		return "RandomGamer";
+	}
+
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new ReflexDetailPanel();
+	}
+
+
+}
