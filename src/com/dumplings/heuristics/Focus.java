9cedf307f44a19b2a8231bd6c67b2f4da7da62b9
luyota <luyota@stanford.edu>
2011-04-26 19:24:51 -0700

    Throws exception in mobility related code.

--- src/com/dumplings/heuristics/Focus.java
+++ src/com/dumplings/heuristics/Focus.java
@@ -3,6 +3,9 @@ package com.dumplings.heuristics;
 import util.statemachine.MachineState;
 import util.statemachine.Role;
 import util.statemachine.StateMachine;
+import util.statemachine.exceptions.GoalDefinitionException;
+import util.statemachine.exceptions.MoveDefinitionException;
+import util.statemachine.exceptions.TransitionDefinitionException;
 
 import com.dumplings.general.PlayerHeuristic;
 
@@ -18,13 +21,11 @@ public class Focus implements PlayerHeuristic {
 	}
 
 	@Override
-	public Integer getScore(MachineState state, Role role) {
-		try {
-			int score = 100 - stateMachine.getLegalMoves(state, role).size();
-			System.out.println("Focus get score: " + score);
-			return score;
-		} catch (Exception e) {			
-		}
-		return 0;
+	public Integer getScore(MachineState state, Role role) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
+		
+		int score = 100 - stateMachine.getLegalMoves(state, role).size();
+		System.out.println("Focus get score: " + score);
+		return score;
+		
 	}
 }
